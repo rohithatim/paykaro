f99d5acc16deacb23aa45417b242bc5ee981e25f
rohithatim <hatimrohit079@gmail.com>
2024-12-19 11:21:46 +0530

    second commit

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://localhost:27017/paytm"
-    );
+    const conn = await mongoose.connect("mongodb://localhost:27017/paykaro");
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
